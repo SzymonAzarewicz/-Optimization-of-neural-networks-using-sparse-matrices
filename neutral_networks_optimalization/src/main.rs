501172ac9195e25a3e1579d2884295c0ef0937a2
190518 <s190518@student.pg.edu.pl>
2024-11-03 12:46:48 +0100

    struktura i implementacja neuronu i warstwy

--- neutral_networks_optimalization/src/main.rs
+++ neutral_networks_optimalization/src/main.rs
@@ -1,3 +1,20 @@
+mod structure;
+use structure::Neuron;
+use structure::Layer;
+
+
 fn main() {
-    println!("Hello, world!");
+
+// Tworzymy warstwę z 3 neuronami, każdy neuron ma 2 wejścia
+    let mut layer = Layer::new(3, 2);
+
+// Przykładowe dane wejściowe
+    let inputs = vec![0.5, -1.5];
+
+// Obliczamy output dla każdego neuronu w warstwie
+    layer.forward(&inputs);
+
+// Wyświetlamy zawartość wszystkich neuronów w warstwie
+    layer.display();
+
 }
---
+++ neutral_networks_optimalization/src/structure.rs
@@ -0,0 +1,99 @@
+use rand::Rng;
+use std::io;
+
+pub struct Neuron{
+    weights: Vec<f64>,
+    bias: f64,
+    output: f64,
+}
+pub struct Layer{
+    neurons: Vec<Neuron>,
+}
+
+impl Neuron{
+    //inicjalizacja
+    pub fn new(num_inputs: usize) -> Self{
+        let mut rng = rand::thread_rng(); // losowy gen
+        let mut weights = Vec::new();
+        let mut bias = 0.0;
+        let output = 0.0;
+    //uzupełnienie
+    for _ in 0..num_inputs {
+            weights.push(rng.gen_range(-1.0..1.0));
+            bias = rng.gen_range(-1.0..1.0);
+        }
+    //zwracanie
+        Neuron { weights, bias, output}
+    }
+
+    //obliczanie wyjścia
+    pub fn forward(&mut self, inputs: &Vec<f64>){
+        let mut sum = 0.0;
+        for (w, i) in self.weights.iter().zip(inputs) {
+            sum += w * i;
+            //self.output = get_activation_function(sum);
+        }
+        sum = sum + self.bias;
+        self.output = get_activation_function(sum);
+    }
+
+     pub fn display(&self) {
+         println!("Neuron:");
+         println!("  Weights: {:?}", self.weights);
+         println!("  Bias: {}", self.bias);
+         println!("  Output: {}", self.output);
+     }
+}
+impl Layer {
+    pub fn new(neurons_count: usize, inputs_count: usize) -> Self {
+        let mut neurons = Vec::with_capacity(neurons_count);
+        for _ in 0..neurons_count {
+            neurons.push(Neuron::new(inputs_count));
+        }
+        Layer { neurons }
+    }
+    pub fn forward(&mut self, inputs: &Vec<f64>) {
+        for neuron in self.neurons.iter_mut() {
+            neuron.forward(inputs);
+        }
+    }
+    pub fn display(&self) {
+        for (i, neuron) in self.neurons.iter().enumerate() {
+            println!("Neuron {}:", i + 1);
+            neuron.display();
+        }
+    }
+}
+
+
+pub fn get_activation_function(sum: f64) ->f64{
+    println!("Choose activation function:");
+    println!("1. ReLU");
+    println!("2. Sigmoid");
+    println!("3. Tanh");
+
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("Failed to read line");
+    match choice.trim(){
+        "1" => relu(sum),
+        "2" => sigmoid(sum),
+        "3" => tanh(sum),
+        _ => {
+            println!("Nieprawidłowy wybór. Domyślnie wybrano sigmoid");
+            sigmoid(sum)
+        }
+    } 
+}
+fn relu(i: f64) -> f64 {
+    i.max(0.0)
+}
+fn sigmoid(i: f64) -> f64 {
+    1.0 / (1.0 + (-i).exp())
+}
+fn tanh(i: f64) -> f64 {
+    i.tanh()
+}
+
+
+
+
